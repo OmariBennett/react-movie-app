5986bb61b6eb6d0058ec6cc4705d67428e0d4d56
OmariBennett <omari.bennett@gmail.com>
2021-09-28 14:31:23 -0400

    A initial setup

---
+++ src/components/App.js
@@ -0,0 +1,48 @@
+/* Movie Database: Entertainment Data Hub
+API Features:
+  Get/Images/Redirect
+Matching
+  Get Data Mactch
+  Get ID Match
+Searching
+  Get Search
+
+Site Features
+  * Signup form
+  * Login form
+  * Search form 
+    - title
+    - ID
+
+  * Display
+    - Title
+    - Descriptions
+    - Date
+      - Year
+      - Month
+    - Genres
+
+  * Chat / Messaging
+    - Send 
+    - Rceive
+
+  * User Porfile
+*/
+
+/* Middleware / Libaries
+  Axios
+  Socket.io
+  Express.js
+  React
+  .Env
+*/
+
+function App() {
+	return (
+		<>
+			<h1>Hola World!</h1>
+		</>
+	);
+}
+
+export default App;
