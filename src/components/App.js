8f8ee41d90ab3cfec0289d8c410bf6ed2ee438e0
OmariBennett <omari.bennett@gmail.com>
2021-09-30 15:32:53 -0400

    Dispaly a Database response

--- src/components/App.js
+++ src/components/App.js
@@ -37,10 +37,30 @@ Site Features
   .Env
 */
 
+// Step: Dispaly a Database response
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
 export default function App() {
+	const [movies, setMovies] = useState([]);
+	useEffect(() => {
+		axios
+			.get(
+				`https://api.themoviedb.org/3/discover/movie?api_key=${process.env.REACT_APP_API_KEY}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_watch_monetization_types=flatrate`,
+			)
+			.then(function (response) {
+				return setMovies([...response.data.results]);
+			});
+	}, []);
+
 	return (
 		<>
 			<h1>Hola World!</h1>
+			<ol>
+				{movies.map((obj, index) => (
+					<li key={index}>{obj.original_title}</li>
+				))}
+			</ol>
 		</>
 	);
 }
